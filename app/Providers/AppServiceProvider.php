d19ac0601ff0697d669e7a4b4c288066c69c65bb
GeorgePrograma <jorge.beristain.hernandez@gmail.com>
2024-09-14 18:42:04 -0600

    Fix provider to Update Laravel 10

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
+//use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Passport::enablePasswordGrant();
+        //Passport::enablePasswordGrant();
     }
 
     /**
